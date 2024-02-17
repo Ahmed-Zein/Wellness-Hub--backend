71844a70347e9e7b34e0cb2bc913778cdcf716ce
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-02-17 21:16:20 +0200

    fix:mongoose

--- config/dev.js
+++ config/dev.js
@@ -1,13 +1,5 @@
 const config = {
-  mongoUrl:
+  mongoUri:
     "mongodb+srv://ahmedzein3652:Ddsl2HuuFYVINK4d@cluster0.aicunll.mongodb.net/?retryWrites=true&w=majority",
 };
-
-module.exports = config;
-// Ddsl2HuuFYVINK4d;
-// mongodb+srv://ahmedzein3652:Ddsl2HuuFYVINK4d@cluster0.aicunll.mongodb.net/
-mongoose.connect(process.env.mongoUri).then((result) => {
-  app.listen(3000, () => {
-    console.log(">> server started");
-  });
-});
+module.exports=config
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
+const mongoose = require("mongoose");
 const config = require("./config/dev");
-// const mongoose = require("mongoose");
-// mongoose.connect(config.mongoUrl);
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri = "mongodb+srv://ahmedzein3652:<password>@cluster0.aicunll.mongodb.net/?retryWrites=true&w=majority";
-const uri = config.mongoUrl;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  console.log("test");
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+mongoose
+  .connect(config.mongoUri, {})
+  .then((result) => {
+      console.log(">> server started");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
