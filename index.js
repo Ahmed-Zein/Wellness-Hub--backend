bafc28d468173c4502b488ac539bb15063a9d93b
Eyad Elkiey <eyadelkiey@hotmail.com>
2024-02-26 14:18:10 +0200

    fixed products route

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/customer", customerRoute);
 app.use("/api/v1/seller", sellerRoute);
-app.use("/api/v1/product", productRoute);
+app.use("/api/v1/products", productRoute);
 app.use("/api/v1/meals", mealsRoute);
 
 app.use((err, req, res, next) => {
