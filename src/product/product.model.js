91cd097e01c9465722ddd6b73997117b1f64dece
Eyad Elkiey <eyadelkiey@hotmail.com>
2024-02-26 14:14:36 +0200

    Updated the Model of the Product

--- src/product/product.model.js
+++ src/product/product.model.js
@@ -32,7 +32,9 @@ const productSchema = new mongoose.Schema({
   rate: [{
     user_ID: {
        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Customer' },
+        ref: 'Customer', 
+        required:true,
+      },
     rate: {
        type: Number, 
        required: true,
@@ -54,21 +56,21 @@ const productSchema = new mongoose.Schema({
        },
     content: { 
       type: String,
-       required: true 
+      required: true 
       }
   }],
 });
 // a middlware  to update the updatedat attribute before saving the product
-// productSchema.pre(`save`, function (next) {
-//   this.UpdatedAt = Date.now();
-//   next();
-// });
+ productSchema.pre(`save`, function (next) {
+   this.UpdatedAt = Date.now();
+   next();
+ });
 
 // Calculate the average rating before saving the product
-//productSchema.pre('save', function (next) {
-  //const totalrate = this.rate.reduce((sum, r) => sum + r.rate, 0); //reduce is a funtion in JS to reduce an array to a single value  and its arrgument is a callback function of 2 parameters the first is the  accumulator that accumlates the results of each iteration (el m5zn el bn5zn feh ) while the r represensents each element of the array that while be accumlated 
-  //this.finalrate = totalrate / this.rate.length || 0; //calculate the average by division and if the rate array is empty it will equal 0
-  //next();
-//});
+productSchema.pre('save', function (next) {
+  const totalrate = this.rate.reduce((sum, r) => sum + r.rate, 0); //reduce is a funtion in JS to reduce an array to a single value  and its arrgument is a callback function of 2 parameters the first is the  accumulator that accumlates the results of each iteration (el m5zn el bn5zn feh ) while the r represensents each element of the array that while be accumlated 
+  this.finalrate = totalrate / this.rate.length || 0; //calculate the average by division and if the rate array is empty it will equal 0
+  next();
+cs});
 
 module.exports = mongoose.model("product", productSchema);
