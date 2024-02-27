430887eddef18ca27a4902a8e1e579f11c3cecd4
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-02-27 23:03:15 +0200

    cleanup

--- src/customer/customer.routes.js
+++ src/customer/customer.routes.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const customerController = require("./customer.controller");
 const { authenticateToken } = require("../common/jwt");
-const { ping } = require("../common/utils");
-const Customer = require("./customer.model");
-const Product = require("../product/product.model");
-const Meal = require("../meals/meal.model");
 
 const router = express.Router();
 
@@ -29,7 +25,5 @@ router.post(
   authenticateToken,
   customerController.addToWishlist
 );
-
-router.get("/test", authenticateToken, ping);
 
 module.exports = router;
