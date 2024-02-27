fee31792ca9a470bc345b66353ed563a84458aa0
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-02-27 23:07:39 +0200

    display wellnessHub instead of test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Test test");
+  res.send("<h1>WellnessHub</h1>");
 });
 
 app.use("/api/v1/customer", customerRoute);
