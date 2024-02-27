b54ee6758894380c4fd9411beed6456c30319fd4
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-02-27 20:54:00 +0200

    move:validation middleware

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,7 +1,28 @@
+const { validationResult } = require("express-validator");
+const logger = require("./logger");
+
 exports.ping = (req, res) => {
   res.status(200).end();
 };
-exports.formatMeal = (meal) => {
+
+// Middleware for handling validation errors
+exports.validationMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = [];
+  errors.array().map((err) => {
+    logger.error(err.msg);
+    extractedErrors.push({ [err.path]: err.msg });
+  });
+
+  return res
+    .status(422)
+    .json({ message: "validation error", errors: extractedErrors });
+};
+
+exports.transformMealToClientFormat = (meal) => {
   return {
     id: meal._id,
     seller: meal.seller,
