f5f8feeb34463c3cb713cc862a5380a3a2614f39
Eyad Elkiey <eyadelkiey@hotmail.com>
2024-03-04 23:05:57 +0200

    order model added

---
+++ src/order/order.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'delivered'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
