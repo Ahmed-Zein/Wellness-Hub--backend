887e82a1d9b1883fdbc31507161864f0562093fa
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-03-05 07:12:06 +0200

    add: separate middlerwares file

---
+++ src/common/middlewares.js
@@ -0,0 +1,22 @@
+const { validationResult } = require("express-validator");
+
+// Middleware for handling validation errors
+exports.validationMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = [];
+  errors.array().map((err) => {
+    logger.error(err.msg);
+    extractedErrors.push({ [err.path]: err.msg });
+  });
+
+  return res
+    .status(422)
+    .json({ message: "validation error", errors: extractedErrors });
+};
+
+exports.ping = (req, res) => {
+  res.status(200).end();
+};
