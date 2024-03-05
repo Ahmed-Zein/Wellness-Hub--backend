65888a9986964ec5f4605b5c138530f0a9be26fb
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-03-05 07:30:25 +0200

    refactor: moving to seller controller

---
+++ src/seller/seller.controller.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcryptjs");
+
+const Seller = require("./seller.model");
+const logger = require("../common/logger");
+const { generateAccessToken } = require("../common/jwt");
+
+exports.register = async (req, res) => {
+  const { name, email, password, SSN, phone, address } = req.body;
+
+  if (!(name && email && password && phone))
+    return res.status(400).send({ message: "missing data" });
+
+  const alreadyExists = await Seller.findOne({ email: email }).exec();
+  if (alreadyExists) {
+    return res.status(400).send({ message: "user already exists" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    if (!hashedPassword) throw Error("server error");
+    const seller = await Seller.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+      SSN: SSN,
+      phone: phone,
+      address: address,
+    });
+
+    await seller.save();
+    const accessToken = generateAccessToken(
+      { _id: seller._id },
+      process.env.TOKEN_SECRET
+    );
+
+    logger.info("seller registerd successfully");
+    res.status(201).send({
+      message: "seller registerd successfully",
+      userId: result._id,
+      token: accessToken,
+    });
+  } catch (e) {
+    logger.error(e);
+    res.status(500).send({ error: e.message });
+  }
+};
+
+exports.getSellerData = async (req, res) => {
+  try {
+    const seller = await require("./seller.model").findById(
+      req.params.sellerid
+    );
+    if (!seller) {
+      return res.status(404).json({ message: "Seller not found" });
+    }
+
+    res.status(200).json({
+      name: seller.name,
+      email: seller.email,
+      phone: seller.phone,
+      meals: seller.meals,
+      products: seller.products,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- src/seller/seller.router.js
+++ src/seller/seller.router.js
@@ -1,56 +1,15 @@
 const express = require("express");
 
-const bcrypt = require("bcryptjs");
 const { body } = require("express-validator");
 
 const Seller = require("./seller.model");
-const UserToken = require("../models/user.token.model");
-const logger = require("../common/logger");
-const { generateAccessToken } = require("../common/jwt");
 const { login, findOneUser } = require("../common/auth");
 const { validationMiddleware } = require("../common/middlewares");
+const { register, getSellerData } = require("./seller.controller");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { name, email, password, SSN, phone, address } = req.body;
-
-  if (!(name && email && password && phone))
-    return res.status(400).send({ message: "missing data" });
-
-  const alreadyExists = await require("./seller.model")
-    .findOne({ email: email })
-    .exec();
-  if (alreadyExists) {
-    return res.status(400).send({ message: "user already exists" });
-  }
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 12);
-    if (!hashedPassword) throw Error("server error");
-    const seller = await require("./seller.model").create({
-      name: name,
-      email: email,
-      password: hashedPassword,
-      SSN: SSN,
-      phone: phone,
-      address: address,
-    });
-
-     await seller.save();
-    const accessToken = generateAccessToken(
-      { _id: seller._id },
-      process.env.TOKEN_SECRET
-    );
-
-    res
-      .status(201)
-      .send({ message: "success", userId: result._id, token:  accessToken});
-  } catch (e) {
-    console.error(e);
-    res.status(500).send({ error: e.message });
-  }
-});
+router.post("/register", register);
 
 router.post(
   "/login",
@@ -68,25 +27,6 @@ router.post(
 );
 
 // Get /sellers  endpoint to get the data of a specific  seller
-router.get("/:sellerid", async (req, res) => {
-  try {
-    const seller = await require("./seller.model").findById(
-      req.params.sellerid
-    );
-    if (!seller) {
-      return res.status(404).json({ message: "Seller not found" });
-    }
-
-    res.status(200).json({
-      name: seller.name,
-      email: seller.email,
-      phone: seller.phone,
-      meals: seller.meals,
-      products: seller.products,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get("/:sellerid", getSellerData);
 
 module.exports = router;
