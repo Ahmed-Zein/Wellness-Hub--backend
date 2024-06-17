efff0d38ad0c726131701f99ca03bdf36ed651c2
Ahmed Zein <ahmedzein3652@gmail.com>
2024-06-18 00:41:19 +0300

    add: config file to be used instead of env vars.

--- config/dev.js
+++ config/dev.js
@@ -1,2 +1,10 @@
-const config = {};
+require("dotenv").config();
+
+const config = {
+  PORT: 3000,
+  MONGO_URI: process.env.MONGO_URI,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  STRIPE_PRIVATE_KEY: process.env.STRIPE_PRIVATE_KEY,
+};
+
 module.exports = config;
