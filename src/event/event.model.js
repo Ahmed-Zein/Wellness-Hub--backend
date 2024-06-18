adb7e6ee4c96258c7d16573f220515dfa8176863
Ahmed Zein <ahmedzein3652@gmail.com>
2024-06-19 02:17:10 +0300

    only user can create events

--- src/event/event.model.js
+++ src/event/event.model.js
@@ -1,49 +1,39 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-    creator:{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: {
-            type: String,
-            enum:['customer', 'seller']
-        },
-        required: true 
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "customer",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  participants: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "customer",
+      },
     },
-    eventName: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 20
-
-    },
-    eventDescription: {
-        type: String,
-        required: true
-    },
-    
-    participants:[{
-        userID: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: {
-                enum:['customer', 'seller']
-            }
-           } 
-    }],
-
-    eventDate:{
-        type: Date,
-        required: true
-    },
-
-    location:{
-        type: String,
-        required:true 
-    }
+  ],
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
 });
 
-
 const Event = mongoose.model("events", eventSchema);
-
 
 module.exports = Event;
